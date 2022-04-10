ffd82c8e468d234e78aba3caa869caaa9dc46eb7
Gabriel Morais <gabmla19@gmail.com>
2022-04-10 13:05:16 -0300

    adding groupSelect for skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,4 +1,5 @@
 import { Character, Monster } from "./input";
+import { Job } from "./job";
 import { getHP, getSP } from "./stats";
 
 export interface FormulaReturn {
@@ -10,6 +11,7 @@ export interface Skill {
   name: string;
   label: string;
   key: string;
+  job: Job | "All";
   isMelee: boolean;
   formula: (character: Character, monster: Monster) => FormulaReturn;
 }
@@ -20,6 +22,7 @@ export const SKILLS: Record<string, Skill> = {
     name: "Auto Attack",
     label: "Auto Attack",
     isMelee: true,
+    job: "All",
     formula: (_character: Character, _monster: Monster) => ({
       percent: 100,
       bonus: 0,
@@ -30,6 +33,7 @@ export const SKILLS: Record<string, Skill> = {
     name: "Triple Attack",
     label: "Triple Attack",
     isMelee: true,
+    job: "Sura",
     formula: (_character: Character, _monster: Monster) => ({
       percent: 200,
       bonus: 0,
@@ -40,6 +44,7 @@ export const SKILLS: Record<string, Skill> = {
     label: "Tiger Cannon (Combo)",
     name: "Tiger Cannon",
     isMelee: true,
+    job: "Sura",
     formula: (character: Character, monster: Monster) => {
       const baseDamage = (0.3 * getHP(character) + 0.15 * getSP(character)) / 2;
       return {
@@ -48,11 +53,26 @@ export const SKILLS: Record<string, Skill> = {
       };
     },
   },
+  // THIRD_FLAME_BOMB: {
+  //   key: "THIRD_FLAME_BOMB",
+  //   label: "Third Flame Bomb",
+  //   name: "Third Flame Bomb",
+  //   isMelee: true,
+  //   job: "Sura",
+  //   formula: (character: Character, monster: Monster) => {
+  //     const baseDamage = 3250 + 0.2 * getHP(character);
+  //     return {
+  //       percent: baseDamage * (character.baseLevel / 100),
+  //       bonus: 0,
+  //     };
+  //   },
+  // },
   CART_CANNON: {
     key: "CART_CANNON",
     label: "Cart Cannon",
     name: "Cart Cannon",
     isMelee: false,
+    job: "Genetic",
     formula: (character: Character, monster: Monster) => {
       // (250 * skillLevel) + (100 * Cart Remodeling skill level)
       const baseDamage = 1250 + 500;
@@ -68,6 +88,7 @@ export const SKILLS: Record<string, Skill> = {
     label: "Solar Burst",
     name: "Solar Burst",
     isMelee: true,
+    job: "Star Emperor",
     formula: (character: Character, monster: Monster) => {
       const baseDamage = 3200;
       return {
@@ -76,19 +97,6 @@ export const SKILLS: Record<string, Skill> = {
       };
     },
   },
-  // THIRD_FLAME_BOMB: {
-  //   key: "THIRD_FLAME_BOMB",
-  //   label: "Third Flame Bomb",
-  //   name: "Third Flame Bomb",
-  //   isMelee: true,
-  //   formula: (character: Character, monster: Monster) => {
-  //     const baseDamage = 3250 + 0.2 * getHP(character);
-  //     return {
-  //       percent: baseDamage * (character.baseLevel / 100),
-  //       bonus: 0,
-  //     };
-  //   },
-  // },
 };
 
 export function getSkill(name: string) {
