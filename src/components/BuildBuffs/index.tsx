6e0f3b3108c6385b28d8d3c259e05b978bdcd44a
gabrielmla <gabmla19@gmail.com>
2022-11-29 23:38:44 -0300

    dynamic buffInputs

--- src/components/BuildBuffs/index.tsx
+++ src/components/BuildBuffs/index.tsx
@@ -1,6 +1,15 @@
 import "./index.css";
-import { Buffs } from "../../data/input";
+import { Buffs, emptyBuffs } from "../../data/input";
 import { BuildBuffCheckBox } from "../BuildCheckBox";
+
+const capitalize = (str: string) => {
+  return str
+    .replace(/([A-Z])/g, " $1")
+    .replace(/^./, function (str) {
+      return str.toUpperCase();
+    })
+    .trim();
+};
 
 const BuildBuffs = () => {
   return (
@@ -11,94 +20,17 @@ const BuildBuffs = () => {
         <b>Build 2</b>
       </div>
       <div className="build-content">
-        <BuildBuffCheckBox
-          label="Magnum Break"
-          getValue={(buffs: Buffs) => buffs.magnumBreak}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            magnumBreak: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Shield Spell"
-          getValue={(buffs: Buffs) => buffs.shieldSpell}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            shieldSpell: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Inspiration"
-          getValue={(buffs: Buffs) => buffs.inspiration}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            inspiration: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Enchant Deadly Poison"
-          getValue={(buffs: Buffs) => buffs.enchantDeadlyPoison}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            enchantDeadlyPoison: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Pyrexia"
-          getValue={(buffs: Buffs) => buffs.pyrexia}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            pyrexia: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Spirit Sphere 15x"
-          getValue={(buffs: Buffs) => buffs.allSpheres}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            allSpheres: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="True Sight"
-          getValue={(buffs: Buffs) => buffs.trueSight}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            trueSight: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Fear Breeze"
-          getValue={(buffs: Buffs) => buffs.fearBreeze}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            fearBreeze: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Unlimit"
-          getValue={(buffs: Buffs) => buffs.unlimit}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            unlimit: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Shadow Warrior"
-          getValue={(buffs: Buffs) => buffs.shadowWarrior}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            shadowWarrior: value,
-          })}
-        />
-        <BuildBuffCheckBox
-          label="Earth Charm"
-          getValue={(buffs: Buffs) => buffs.earthCharm}
-          updateValue={(value: boolean) => (prevState: Buffs) => ({
-            ...prevState,
-            earthCharm: value,
-          })}
-        />
+        {Object.getOwnPropertyNames(emptyBuffs).map((buff) => (
+          <BuildBuffCheckBox 
+            key={buff}
+            label={capitalize(buff)}
+            getValue={(buffs: Buffs) => buffs[buff as keyof Buffs]}
+            updateValue={(value: boolean) => (prevState: Buffs) => ({
+              ...prevState,
+              [buff]: value,
+            })}
+          />
+        ))}
       </div>
     </div>
   );
